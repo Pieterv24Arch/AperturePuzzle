b7559a0222b8b264c253fbdb323776e540cc6ed8
Richard Boomsma <richardboomsma@gmail.com>
2016-11-02 16:11:52 +0100

    walking

--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -6,8 +6,9 @@ public class GameManager : MonoBehaviour
 {
     public Transform SelectionCube;
     public Camera SceneCamera;
+    public PlayerController playerController;
 
-    void FixedUpdate()
+    void Update()
     {
         RaycastHit hit;
         if (Physics.Raycast(SceneCamera.ScreenPointToRay(Input.mousePosition), out hit, 50))
@@ -16,14 +17,12 @@ public class GameManager : MonoBehaviour
                 SelectionCube.gameObject.SetActive(true);
 
             Vector3 target = hit.point;
-            target.x = Mathf.Round(target.x+0.5f)-0.5f;
-            target.z = Mathf.Round(target.z+0.5f)-0.5f;
-            SelectionCube.position = target + new Vector3(0,0.025f,0);
+            target.x = Mathf.Round(target.x + 0.5f) - 0.5f;
+            target.z = Mathf.Round(target.z + 0.5f) - 0.5f;
+            SelectionCube.position = target + new Vector3(0, 0.025f, 0);
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-
-            }
+                playerController.MoveTo(target);
         }
         else if (SelectionCube.gameObject.activeSelf)
             SelectionCube.gameObject.SetActive(false);
--- TowerDefense/Assets/Scripts/PlayerController.cs
+++ TowerDefense/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    private NavMeshAgent agent;
+    public Animator animator;
+
+    private bool isMoving = false;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     public void MoveTo(Vector3 position)
     {
+        agent.SetDestination(position);
+    }
 
+    void FixedUpdate()
+    {
+        if(agent.remainingDistance < 0.1f && isMoving)
+        {
+            isMoving = false;
+            animator.SetBool("isWalking", isMoving);
+        }
+        else if(agent.remainingDistance >= 0.1f && !isMoving)
+        {
+            isMoving = true;
+            animator.SetBool("isWalking", isMoving);
+        }
     }
 }
