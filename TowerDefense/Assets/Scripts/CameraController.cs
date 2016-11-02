3474d0a911c8c34c42baa6f224bfe3be61d34494
Pieter Verweij <pieter1998.verweij@gmail.com>
2016-11-02 17:25:55 +0100

    Started working on camera control

---
+++ TowerDefense/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        
+	    }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.A))
+	    {
+	        transform.Translate(Vector3.left);
+	    }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right);
+        }
+        transform.LookAt(Vector3.zero);
+    }
+}
