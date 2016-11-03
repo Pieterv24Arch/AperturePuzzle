90e1001acd894bac2b7e2fdf6dbc16338f149b7e
Richard Boomsma <richardboomsma@gmail.com>
2016-11-03 14:10:39 +0100

    elevator

--- TowerDefense/Assets/Scripts/Elevator.cs
+++ TowerDefense/Assets/Scripts/Elevator.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    public float speed = 1;
 
+    public Vector3 bottomPosition;
+    public Vector3 topPosition;
+
+    private float lastTimeSwitch;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, topPosition, speed * Time.deltaTime);
+    }
 }
