9a9f4a4ae1f232c189ab931e0d70d4683c9fc6a1
Pieter Verweij <pieter1998.verweij@gmail.com>
2016-11-03 13:36:24 +0100

    Camera fixes

--- TowerDefense/Assets/Scripts/CameraController.cs
+++ TowerDefense/Assets/Scripts/CameraController.cs
@@ -2,47 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VR.WSA.Persistence;
 
 public class CameraController : MonoBehaviour
 {
-    public Vector3 target = Vector3.zero;
-	
-	// Update is called once per frame
+    public float distance = 15;
+    public Vector3 angle;
+    public Transform targetObject;
+    
 	void Update ()
 	{
-        if (Input.GetKey(KeyCode.W))
-        {
-            if(Vector3.Angle(new Vector3(transform.position.x, 0, transform.position.z), transform.position) < 85)
-                transform.RotateAround(target, transform.right, 1);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            if(transform.position.y > 1)
-                transform.RotateAround(target, transform.right, -1);
-        }
-        if (Input.GetKey(KeyCode.A))
-	    {
-            transform.RotateAround(target, Vector3.up, 1);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.RotateAround(target, Vector3.up, -1);
-        }
-	    if (Input.GetKey(KeyCode.Q))
-	    {
-            if(Physics.Raycast(transform.position, transform.forward))
-	        transform.Translate(transform.forward * 0.1F, Space.World);
-	    }
-	    if (Input.GetKey(KeyCode.E))
-	    {
-            transform.Translate(-transform.forward * 0.1F, Space.World);
-        }
-        transform.LookAt(target);
-    }
-
-    Vector3 CalcNewRotPosition(Vector3 center, Vector3 position)
-    {
-
-        return Vector3.zero;
-    }
+	    angle.y += Input.GetKey(KeyCode.A) ? 1 : Input.GetKey(KeyCode.D) ? -1 : 0;
+	    transform.eulerAngles = angle;
+        transform.position = targetObject.position + (transform.rotation * Vector3.back * distance);
+	}
 }
